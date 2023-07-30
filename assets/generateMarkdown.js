623f733988bfdac95fbb996f7e50275af383c250
will sherman <willsherman771@gmail.com>
2023-07-30 17:57:09 -0400

    finished js and gM

---
+++ assets/generateMarkdown.js
@@ -0,0 +1,40 @@
+function generateBadge(license) {
+    if (license !== 'none') {
+        return `(https://img.shields.io/badge/${license}-blue.svg)`;
+    }
+    return'';
+}
+
+// generating markdown
+function generateMarkdown(data) {
+    return `# ${data.title} 
+ by Matthew Shuman ${renderLicenseBadge(data.license)},
+
+
+  -------------------------
+  ## Table of Contents
+  * [Description](*description)
+  * [Link] (*link)
+  * [How to Use This Application](*usage)
+  * [Installation] (*installation)
+  * [Contributors](#contributors)
+  * [Questions](#questions)
+  -------------------------
+  ## Description
+  ${data.description}
+  ## Deployed Application URL
+  ${data.link}
+  ## How to Use This Application:
+  ${data.function}
+  ## Dependencies 
+  ${data.require}
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  ${data.test}
+  ## Questions
+  Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
+  `;
+  }
+  
+  module.exports = generateMarkdown;
